f52a87b8daf314997743fa62bc1051809eaf9c9d
lorenzobianchi <lorenzo.bianchi@inmagik.com>
2016-05-10 16:53:03 +0200

    first commit

---
+++ website/pages.py
@@ -0,0 +1,53 @@
+#TEMPLATES WILL BE INFERRED BY NAMES
+
+SITE_PAGES = {
+
+    'home' : {
+        'url' : '',
+    },
+
+    'news' : {
+        'url' : 'news',
+    },
+
+    'contact' : {
+        'url' : 'contact',
+    },
+
+    'test' : {
+        'url' : 'test',
+    },
+
+    'tour' : {
+        'url' : 'tour',
+    },
+
+    'learn' : {
+        'url' : 'learn',
+    },
+
+    'servizi' : {
+        'url' : 'servizi',
+    },
+
+    'contatti' : {
+        'url' : 'contatti',
+    },
+
+    'login' : {
+        'url' : 'login',
+    },
+
+    # 'items' : {
+    #     'url' : 'items',
+    #     'extra_modules' : []
+    # },
+
+    # 'item-detail' : {
+    #     'url' : 'items/(?P<pk>\w+)',
+
+    # },
+
+
+
+}
